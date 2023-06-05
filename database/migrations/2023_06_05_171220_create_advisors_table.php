11d533eb11a11718da9bdd69b8af6852ea8f40f9
Elian Gonzalez <eliangls@hotmail.com>
2023-06-05 11:22:07 -0600

    Config - Advisors table

--- database/migrations/2023_06_05_171220_create_advisors_table.php
+++ database/migrations/2023_06_05_171220_create_advisors_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('advisors', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->integer('num_advisors');
             $table->timestamps();
         });
     }
