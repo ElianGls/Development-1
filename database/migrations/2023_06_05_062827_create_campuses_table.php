c86234f9195100e1dd520885e93a47df56cb6e2f
Elian Gonzalez <eliangls@hotmail.com>
2023-06-05 00:29:16 -0600

    Config - Campuses table

--- database/migrations/2023_06_05_062827_create_campuses_table.php
+++ database/migrations/2023_06_05_062827_create_campuses_table.php
@@ -12,8 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('campuses', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            Schema::create('campuses', function (Blueprint $table) {
+                $table->id();
+                $table->string('name_campuses', 255);
+                $table->string('address_campuses', 255);
+                $table->string('phone_campuses');
+                $table->string('email_campuses');
+                $table->timestamps();
+            });
         });
     }
 
