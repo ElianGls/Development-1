a889ddbed3333ce6b7477e19b38f3011bf05cc54
Elian Gonzalez <eliangls@hotmail.com>
2023-06-05 00:12:00 -0600

    Config - Schedules table

--- database/migrations/2023_06_05_060940_create_schedules_table.php
+++ database/migrations/2023_06_05_060940_create_schedules_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('schedules', function (Blueprint $table) {
             $table->id();
+            $table->enum('weekday', ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo']);
+            $table->time('start_time');
+            $table->time('end_time');
             $table->timestamps();
         });
     }
