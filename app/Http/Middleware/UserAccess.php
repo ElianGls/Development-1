f445ab3f6b91c769f18025af9b48b3dd1962800e
Elian Gonzalez <eliangls@hotmail.com>
2023-06-04 23:34:13 -0600

    Update - Function handle

--- app/Http/Middleware/UserAccess.php
+++ app/Http/Middleware/UserAccess.php
@@ -1,20 +1,27 @@
 <?php
-
+ 
 namespace App\Http\Middleware;
-
+ 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
-
+ 
 class UserAccess
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next): Response
+ 
+    public function handle(Request $request, Closure $next, $userType)
     {
-        return $next($request);
+        if(auth()->user()->type == $userType){
+            return $next($request);
+        }
+            
+        return response()->json(['You do not have permission to access for this page.']);
+        /* return response()->view('errors.check-permission'); */
     }
 }
