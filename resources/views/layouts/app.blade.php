17f01b611149e8b30e47baf03d0b1a06a2d65619
Elian Gonzalez <eliangls@hotmail.com>
2023-06-05 00:34:06 -0600

    Create - Campuses reference

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,7 +39,7 @@
                                 <li><a class="dropdown-item" href="#">Usuarios</a></li>
                                 <li><hr class="dropdown-divider"></li>
                                 <li><a class="dropdown-item" href="{{ route('schedules.index') }}">Horario</a></li>
-                                <li><a class="dropdown-item" href="#">Campus</a></li>
+                                <li><a class="dropdown-item" href="{{ route('campuses.index') }}">Campus</a></li>
                                 <li><a class="dropdown-item" href="#">Departamento  </a></li>
                             </ul>
                     </li>
